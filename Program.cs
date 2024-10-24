42a28ebc34bcfb0b7fe15b127114ac6be1dd3d5d
Anthony Renwick <renwickman@gmail.com>
2024-10-24 12:24:03 -0500

    First Commit of Space Expedition

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class SpaceExpedition
+{
+  public static void Main(string[] args){
+    bool isAtmosphereBreathable = true;
+    bool isGravityStable = false;
+    bool resourcesSufficient = true;
+
+    bool isHabitable = isAtmosphereBreathable && isGravityStable;
+    Console.WriteLine(isHabitable);
+
+    bool suitableForExpansion = isGravityStable && resourcesSufficient;
+    Console.WriteLine(suitableForExpansion);
+
+    int currentCrew = 5;
+    int maxCrewCapacity = 10;
+
+    bool canAcceptMoreCrew = currentCrew < maxCrewCapacity;
+    Console.WriteLine(canAcceptMoreCrew);
+  }
+}
